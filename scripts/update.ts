a44b0ce36cdd1b44b3503045a600d1466e67f6c0
Arjun Chib <arjunpatrickchib@gmail.com>
2023-02-06 20:17:43 -0500

    Use global application commands

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,4 +1,10 @@
 import { updateCommands } from "@blurp/node";
 import commands from "../src/commands.js";
 
-await updateCommands(commands);
+if (process.env.DEVELOPMENT === "true") {
+  await updateCommands(commands);
+  await updateCommands({ commands: [], global: true });
+} else {
+  await updateCommands({ commands, global: true });
+  await updateCommands({ commands: [], global: false });
+}
