5727ad1b260bb838777eae0d394587ae43212c58
Arjun Chib <arjunpatrickchib@gmail.com>
2023-03-18 01:38:13 -0400

    Override broken updateCommands types

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,10 +1,14 @@
+import { CommandModule } from "@blurp/common/core";
 import { updateCommands } from "@blurp/node";
 import commands from "../src/commands.js";
 
 if (process.env.DEVELOPMENT === "true") {
-  await updateCommands(commands);
+  await updateCommands(commands as unknown as CommandModule[]);
   await updateCommands({ commands: [], global: true });
 } else {
-  await updateCommands({ commands, global: true });
+  await updateCommands({
+    commands: commands as unknown as CommandModule[],
+    global: true,
+  });
   await updateCommands({ commands: [], global: false });
 }
